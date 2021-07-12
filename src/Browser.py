aba24fb05a3c1dd8ae2940bd94bb01c9a0d7d96c
Michael Santos <michael-santos@outlook.com.br>
2021-07-12 11:55:42 -0300

    Add sketch for EpicRpg and Browser

---
+++ src/Browser.py
@@ -0,0 +1,6 @@
+class Browser:
+    def login(self, username, password):
+        pass
+
+    def execute_command(self, command):
+        pass
---
+++ src/EpicRPG.py
@@ -0,0 +1,27 @@
+class EpicRpg:
+    # Xp farm
+    def adventure(self):
+        pass
+
+    def hunt(self):
+        pass
+
+    def heal(self):
+        pass
+
+    # Resources farm
+    def chop(self):
+        pass
+
+    def fish(self):
+        pass
+
+    # Recorrency
+    def daily(self):
+        pass
+
+    def weekly(self):
+        pass
+    
+    def vote(self):
+        pass
